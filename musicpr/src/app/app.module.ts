9515ba28756480e1ab7dd61849b66be8c8624810
Pierluca Masiello <pierlucamasiello@hotmail.it>
2019-07-07 18:03:26 +0200

    aggiunte pagine,mappa,html

--- musicpr/src/app/app.module.ts
+++ musicpr/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +19,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    NativeGeocoder,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- musicpr/src/app/mappa/mappa.page.ts
+++ musicpr/src/app/mappa/mappa.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild, OnInit, ElementRef} from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderReverseResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+
+declare var google;
+
 
 @Component({
   selector: 'app-mappa',
@@ -7,9 +12,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MappaPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  address: string;
 
-  ngOnInit() {
+  constructor(
+      private geolocation: Geolocation,
+      private nativeGeocoder: NativeGeocoder) {
   }
 
+
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      const latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      const mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      this.getAddressFromCoords(resp.coords.latitude, resp.coords.longitude);
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      this.map.addListener('tilesloaded', () => {
+        console.log('accuracy', this.map);
+        this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng());
+      });
+
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  getAddressFromCoords(lattitude, longitude) {
+    console.log('getAddressFromCoords ' + lattitude + ' ' + longitude);
+    const options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+
+    this.nativeGeocoder.reverseGeocode(lattitude, longitude, options)
+        .then((result: NativeGeocoderReverseResult[]) => {
+          this.address = '';
+          const responseAddress = [];
+          for (const [key, value] of Object.entries(result[0])) {
+            if (value.length > 0) {
+              responseAddress.push(value);
+            }
+
+          }
+          responseAddress.reverse();
+          for (const value of responseAddress) {
+            this.address += value + ', ';
+          }
+          this.address = this.address.slice(0, -2);
+        })
+        .catch((error: any) => {
+          this.address = 'Address Not Available!';
+        });
+
+  }
 }
+
+
+
+
+
+

1a547aa2dd42425d730f2517ee7027ee4750c4e4
Pierluca Masiello <pierlucamasiello@hotmail.it>
2019-07-10 15:43:16 +0200

    fix comments lista.service

--- musicpr/src/app/services/lista.service.ts
+++ musicpr/src/app/services/lista.service.ts
@@ -1,37 +1,23 @@
-/* ccc
-
-import {HttpClient} from '@angular/common/http';
-import {Injectable} from '@angular/core';
-
-
-import {URL} from '../constants';
-import {Evento} from '../model/evento.model';
-import {Observable} from 'rxjs';
-
-@Injectable({
-    providedIn: 'root'
-})
-
-export class ListaService {
-
-    constructor(private http: HttpClient) {
-    }
-
-    list(): Observable<Evento[]> {
-        return this.http.get<Evento[]>(URL.Evento);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
- */
+// ccc
+// ccc
+// import {HttpClient} from '@angular/common/http';
+// import {Injectable} from '@angular/core';
+// import {Injectable} from '@angular/core';
+// import {Injectable} from '@angular/core';
+// import {URL} from '../constants';
+// import {Evento} from '../model/evento.model';
+// import {Observable} from 'rxjs';
+// import {Observable} from 'rxjs';
+// @Injectable({
+//    providedIn: 'root'
+// })
+// })
+// export class ListaService {
+// export class ListaService {
+//    constructor(private http: HttpClient) {
+//    }
+//    }
+//    list(): Observable<Evento[]> {
+//        return this.http.get<Evento[]>(URL.Evento);
+//    }
+// }

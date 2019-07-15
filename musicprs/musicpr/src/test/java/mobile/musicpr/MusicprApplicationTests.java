40b9b5f3a8bf0bd43ef106cdce7ecb31550ca1e4
axl9369 <marco.calabrese2@student.univaq.it>
2019-07-15 23:03:19 +0200

    creazione  package server side

---
+++ musicprs/musicpr/src/test/java/mobile/musicpr/MusicprApplicationTests.java
@@ -0,0 +1,16 @@
+package mobile.musicpr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MusicprApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

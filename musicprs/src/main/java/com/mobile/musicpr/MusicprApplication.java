72dfff1e445b52cdd24be5007c39191867be4adc
axl9369 <marco.calabrese2@student.univaq.it>
2019-07-16 22:17:02 +0200

    aggiornamento

--- musicprs/src/main/java/com/mobile/musicpr/MusicprApplication.java
+++ musicprs/src/main/java/com/mobile/musicpr/MusicprApplication.java
@@ -1,4 +1,4 @@
-package mobile.musicpr;
+package com.mobile.musicpr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- musicprs/src/test/java/com/mobile/musicpr/MusicprApplicationTests.java
+++ musicprs/src/test/java/com/mobile/musicpr/MusicprApplicationTests.java
@@ -1,4 +1,4 @@
-package mobile.musicpr;
+package com.mobile.musicpr;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
